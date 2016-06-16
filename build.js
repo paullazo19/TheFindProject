66d8603df7026859a0b3550977ca48566e5b01c4
Paul Lazo <paullazo19@gmail.com>
2016-06-16 14:09:54 -0500

    changed root path to createRoutePath component for testing

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ import CreateRoutePath from './modules/CreateRoutePath'
 
 render((
   <Router history={browserHistory}>
-    <Route path="/" component={App}/>
+    <Route path="/" component={CreateRoutePath}/>
     <Route path="/landing" component={Landing}/>
     <Route path="/createRouteLabel" component={CreateRouteLabel}/>
     <Route path="/createRoutePath" component={CreateRoutePath}/>
